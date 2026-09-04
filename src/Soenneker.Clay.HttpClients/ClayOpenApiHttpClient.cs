820208dcf2aaa2cf8c7ce7c86ee0d5482dbad7f0
soenneker <jake@soenneker.com>
2026-09-03 22:01:59 -0500

    Add interface inheritdoc references

--- src/Soenneker.Clay.HttpClients/ClayOpenApiHttpClient.cs
+++ src/Soenneker.Clay.HttpClients/ClayOpenApiHttpClient.cs
@@ -11,6 +11,7 @@ using Soenneker.Utils.HttpClientCache.Abstract;
 
 namespace Soenneker.Clay.HttpClients;
 
+/// <inheritdoc cref="IClayOpenApiHttpClient" />
 public sealed class ClayOpenApiHttpClient : IClayOpenApiHttpClient
 {
     private readonly IHttpClientCache _httpClientCache;
